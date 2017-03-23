62ee6ea90f2679b7628045cd7363d73a1f299828
sssyy <sunyakun1202@gmail.com>
2017-03-23 21:50:50 +0800

    create ten html file

--- createFile.sh
+++ createFile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 path=./createHtml
-[ -d $path]  || mkdir $path
+[ -d $path ]  || mkdir $path
 for i in `seq 10`
 	do
-		char=`echo $RANDOM | md5sum | cut -c 2-11 | tr[0-9] [a-j]`
+		char=`echo $RANDOM | md5sum | cut -c 2-11 | tr [0-9] [a-j]`
 		touch $path/${char}.html
 	done
